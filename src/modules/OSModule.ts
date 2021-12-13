16064ca9270f5dc17e41a7c351bda7e10b55f44a
Raxdiam <Raxdiam@users.noreply.github.com>
2021-12-13 16:09:13 -0500

    Add open/save file and folder select dialog methods

--- src/modules/OSModule.ts
+++ src/modules/OSModule.ts
@@ -22,4 +22,16 @@ export class OSModule extends PhotinoModule {
   cmd(command: string): Promise<string> {
     return this.send(this.cmd.name, command);
   }
+
+  showOpenFile(title: string, filters: string[], multiSelect: boolean = false, defaultPath: string = null): Promise<string> {
+    return this.send(this.showOpenFile.name, title, filters, multiSelect, defaultPath);
+  }
+
+  showSaveFile(title: string, filters: string[], defaultPath: string = null): Promise<string> {
+    return this.send(this.showSaveFile.name, title, filters, defaultPath);
+  }
+
+  showSelectFolder(title: string, defaultPath: string = null): Promise<string> {
+    return this.send(this.showSelectFolder.name, title, defaultPath);
+  }
 }
